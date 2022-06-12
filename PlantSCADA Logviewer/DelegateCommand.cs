d7ba6aedd851fb4da7d02977f0d746d303036e2e
Antonio Girasella <a.girasella@gmail.com>
2022-06-12 19:55:23 +0200

    Building ViewModel base classes

---
+++ PlantSCADA Logviewer/DelegateCommand.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace PlantSCADA_Logviewer
+{
+ public class DelegateCommand : ICommand
+    {
+        private readonly Action _execute;
+        private readonly Func<bool> _canExecute;
+
+        public DelegateCommand(Action execute, Func<bool> canExecute = null)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null || _canExecute();
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute();
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public void RaiseCanExecuteChanged()
+        {
+            var handler = CanExecuteChanged;
+            if (handler != null)
+                handler(this, EventArgs.Empty);
+        }
+    }
+}
---
+++ PlantSCADA Logviewer/MainViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlantSCADA_Logviewer
+{
+    internal class MainViewModel : ViewModelBase
+    {
+        string _logsPath;
+
+        public string LogsPath
+        {
+            get
+            {
+                return _logsPath;
+            }
+            set { 
+                _logsPath = value; 
+                OnPropertyChanged();
+            
+            }
+        }
+    }
+}
